b74e1f478a633d0d2b23c3729565fa276a60187f
AJHassall <ajhassall2000@gmail.com>
2024-07-01 22:47:41 +0100

    initial commit

---
+++ exercises/enums/enums1.rs
@@ -0,0 +1,20 @@
+// enums1.rs
+//
+// No hints this time! ;)
+
+#[derive(Debug)]
+enum Message {
+    // TODO: define a few types of messages as used below
+
+    Quit = 0,
+    Echo,
+    Move,
+    ChangeColor
+}
+
+fn main() {
+    println!("{:?}", Message::Quit);
+    println!("{:?}", Message::Echo);
+    println!("{:?}", Message::Move);
+    println!("{:?}", Message::ChangeColor);
+}
---
+++ exercises/functions/functions1.rs
@@ -0,0 +1,12 @@
+// functions1.rs
+//
+// Execute `rustlings hint functions1` or use the `hint` watch subcommand for a
+// hint.
+
+
+fn call_me() {
+    println!("I am being called!");
+}
+fn main() {
+    call_me();
+}
